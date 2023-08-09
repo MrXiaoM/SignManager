f8acc9030023ebb1b31f353479588e7fc8d4185e
MrXiaoM <coolxiaom95@gmail.com>
2023-08-09 16:08:05 +0800

    initialize repo

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CsharpJson;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SignManager
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
