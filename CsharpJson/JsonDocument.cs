d5b4005763c5b5879608d3258d00735b8a9ae5b7
MrXiaoM <coolxiaom95@gmail.com>
2023-08-10 17:03:42 +0800

    remove UTF-7 support (deprecated)

--- CsharpJson/JsonDocument.cs
+++ CsharpJson/JsonDocument.cs
@@ -36,7 +36,6 @@ namespace CsharpJson
             Default,
             ASCII,
             Unicode,
-            UTF7,
             UTF8,
             UTF32,
             BigEndianUnicode
@@ -201,8 +200,6 @@ namespace CsharpJson
                     return FromString(Encoding.ASCII.GetString(json));
                 case JsonEncoding.Unicode:
                     return FromString(Encoding.Unicode.GetString(json));
-                case JsonEncoding.UTF7:
-                    return FromString(Encoding.UTF7.GetString(json));
                 case JsonEncoding.UTF8:
                     return FromString(Encoding.UTF8.GetString(json));
                 case JsonEncoding.UTF32:
@@ -260,8 +257,6 @@ namespace CsharpJson
                     return Encoding.ASCII.GetBytes(jsonstr);
                 case JsonEncoding.Unicode:
                     return Encoding.Unicode.GetBytes(jsonstr);
-                case JsonEncoding.UTF7:
-                    return Encoding.UTF7.GetBytes(jsonstr);
                 case JsonEncoding.UTF8:
                     return Encoding.UTF8.GetBytes(jsonstr);
                 case JsonEncoding.UTF32:
